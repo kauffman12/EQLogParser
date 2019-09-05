23815f12173e2399d4661222f008f4a4975c7ffc
John Kauffman <jksoftwarehelp@gmail.com>
2019-09-05 18:57:17 -0400

    code cleanup tool

--- EQLogParser/src/ui/breakdown/HealingBreakdown.xaml.cs
+++ EQLogParser/src/ui/breakdown/HealingBreakdown.xaml.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Threading;
 
