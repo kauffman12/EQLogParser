f26ef49aa0067e8acfc8594cf28ccf6facaf7b89
John Kauffman <jksoftwarehelp@gmail.com>
2021-03-22 01:46:39 -0400

    added commas to number formatting for tables

---
+++ EQLogParser/src/ui/control/NumberFormatConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace EQLogParser
+{
+  public class NumberFormatConverter : IValueConverter
+  {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      return $"{value:n0}";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      if (value is string)
+      {
+        if (!double.TryParse((string)value, NumberStyles.AllowThousands, CultureInfo.CurrentCulture, out double decValue))
+        {
+          decValue = 0;
+        }
+        return decValue;
+      }
+      return 0;
+    }
+  }
+}
