51111ffb4facabfb77519d238f5a063df60634c9
John Kauffman <jksoftwarehelp@gmail.com>
2022-07-22 04:40:00 -0400

    small fixes

--- EQLogParser/src/ui/log/SpellDamageStatsViewer.xaml.cs
+++ EQLogParser/src/ui/log/SpellDamageStatsViewer.xaml.cs
@@ -8,8 +8,6 @@ using System.Globalization;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
-using System.Windows.Media;
 
 namespace EQLogParser
 {
