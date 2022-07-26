2b8c4c599e84c809714629cbfca6711176c5cfc6
John Kauffman <jksoftwarehelp@gmail.com>
2022-07-26 01:28:06 -0400

    code cleanup tool

--- MaterialLightCustom/Properties/AssemblyInfo.cs
+++ MaterialLightCustom/Properties/AssemblyInfo.cs
@@ -6,8 +6,6 @@
 // applicable laws. 
 #endregion
 using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
@@ -40,11 +38,11 @@ using System.Windows;
 
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
-    //(used if a resource is not found in the page, 
-    // or application resource dictionaries)
+                                     //(used if a resource is not found in the page, 
+                                     // or application resource dictionaries)
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-    //(used if a resource is not found in the page, 
-    // app, or any theme specific resource dictionaries)
+                                              //(used if a resource is not found in the page, 
+                                              // app, or any theme specific resource dictionaries)
 )]
 
 
