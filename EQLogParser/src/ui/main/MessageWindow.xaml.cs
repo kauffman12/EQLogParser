f1704b34fb938aa299b723fae11a16b09758bcc4
John Kauffman <jksoftwarehelp@gmail.com>
2022-08-29 11:47:45 -0400

    added potential healing field and replaced message box use with custom window

---
+++ EQLogParser/src/ui/main/MessageWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using FontAwesome5;
+using Syncfusion.Windows.Shared;
+
+namespace EQLogParser
+{
+  /// <summary>
+  /// Interaction logic for MessageWindow.xaml
+  /// </summary>
+  public partial class MessageWindow : ChromelessWindow
+  {
+    public bool IsYesClicked = false;
+
+    public MessageWindow(string text, string caption, bool question = false)
+    {
+      MainActions.SetTheme(this, MainWindow.CurrentTheme);
+      InitializeComponent();
+      textBlock.Text = text;
+      Title = caption;
+
+      var brush = question ? "EQMenuIconBrush" : "EQWarnForegroundBrush";
+      iconImage.SetResourceReference(ImageAwesome.ForegroundProperty, brush);
+      iconImage.Icon = question ? EFontAwesomeIcon.Solid_QuestionCircle : EFontAwesomeIcon.Solid_ExclamationTriangle;
+      if (question)
+      {
+        okButton.Content = "No";
+        yesButton.Visibility = System.Windows.Visibility.Visible;
+      }
+    }
+
+    private void ButtonOkClick(object sender, System.Windows.RoutedEventArgs e)
+    {
+      Close();
+    }
+
+    private void ButtonYesClick(object sender, System.Windows.RoutedEventArgs e)
+    {
+      IsYesClicked = true;
+      Close();
+    }
+  }
+}
