d2fb787fa7ca6f0ed99313177a88c8d8f36a1bfe
John Kauffman <jksoftwarehelp@gmail.com>
2022-08-29 18:32:41 -0400

    added potential healing column, new message windows, and save selected fights

--- EQLogParser/src/ui/main/MessageWindow.xaml.cs
+++ EQLogParser/src/ui/main/MessageWindow.xaml.cs
@@ -10,21 +10,35 @@ namespace EQLogParser
   {
     public bool IsYesClicked = false;
 
-    public MessageWindow(string text, string caption, bool question = false)
+    public MessageWindow(string text, string caption, bool question = false, bool save = false)
     {
       MainActions.SetTheme(this, MainWindow.CurrentTheme);
       InitializeComponent();
       textBlock.Text = text;
       Title = caption;
 
-      var brush = question ? "EQMenuIconBrush" : "EQWarnForegroundBrush";
-      iconImage.SetResourceReference(ImageAwesome.ForegroundProperty, brush);
-      iconImage.Icon = question ? EFontAwesomeIcon.Solid_QuestionCircle : EFontAwesomeIcon.Solid_ExclamationTriangle;
+      var brush = "EQWarnForegroundBrush";
+      var image = EFontAwesomeIcon.Solid_ExclamationTriangle;
+
       if (question)
       {
+        brush = "EQMenuIconBrush";
+        image = EFontAwesomeIcon.Regular_QuestionCircle;
         okButton.Content = "No";
         yesButton.Visibility = System.Windows.Visibility.Visible;
       }
+
+      if (save)
+      {
+        brush = "EQMenuIconBrush";
+        image = EFontAwesomeIcon.Solid_Save;
+        okButton.Visibility = System.Windows.Visibility.Hidden;
+        Height -= 15;
+        Width -= 30;
+      }
+
+      iconImage.SetResourceReference(ImageAwesome.ForegroundProperty, brush);
+      iconImage.Icon = image;
     }
 
     private void ButtonOkClick(object sender, System.Windows.RoutedEventArgs e)
