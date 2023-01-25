479f9f5c672c5311d9f48f676d1fb160e9ffd3a5
John Kauffman <jksoftwarehelp@gmail.com>
2023-01-24 20:25:34 -0500

    updated app.xaml
    Former-commit-id: ccffbb61e18ff181bdb36f681b3dfac092b55806

---
+++ EQLogParser/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace EQLogParser
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application
+  {
+    public App()
+    {
+    }
+  }
+}
