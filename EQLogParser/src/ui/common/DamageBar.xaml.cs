544c1794ac6cda0f1aa4cbc98992988e410745a9
John Kauffman <jksoftwarehelp@gmail.com>
2023-02-09 22:29:18 -0500

    cleanup
    Former-commit-id: 51804d4b4e155f0eadb2a009138d180263078e69

--- EQLogParser/src/ui/common/DamageBar.xaml.cs
+++ EQLogParser/src/ui/common/DamageBar.xaml.cs
@@ -24,8 +24,8 @@ namespace EQLogParser
         this.progress.Margin = new System.Windows.Thickness(0, 16, 0, 0);
         this.classImage.Margin = new System.Windows.Thickness(0, 0, 0, 4);
         this.player.Margin = new System.Windows.Thickness(4, 1, 0, 2);
-        this.damage.Margin = new System.Windows.Thickness(0, 1, -7, 2);
-        this.dps.Margin = new System.Windows.Thickness(0, 1, 3, 2);
+        this.damage.Margin = new System.Windows.Thickness(0, 1, -12, 2);
+        this.dps.Margin = new System.Windows.Thickness(0, 1, -2, 2);
         this.time.Margin = new System.Windows.Thickness(0, 1, 2, 2);
       }
       else
@@ -33,8 +33,8 @@ namespace EQLogParser
         this.progress.Margin = new System.Windows.Thickness(0, 0, 0, 0);
         this.classImage.Margin = new System.Windows.Thickness(0, 2, 0, 4);
         this.player.Margin = new System.Windows.Thickness(4, 0, 0, 1);
-        this.damage.Margin = new System.Windows.Thickness(0, 0, -7, 1);
-        this.dps.Margin = new System.Windows.Thickness(0, 0, 3, 1);
+        this.damage.Margin = new System.Windows.Thickness(0, 0, -12, 1);
+        this.dps.Margin = new System.Windows.Thickness(0, 0, -2, 1);
         this.time.Margin = new System.Windows.Thickness(0, 0, 2, 1);
       }
     }
