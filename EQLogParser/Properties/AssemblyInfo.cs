59355dce9d98c49c980ee42b14a5d2ad6c01f552
John Kauffman <jksoftwarehelp@gmail.com>
2023-11-02 00:58:16 -0400

    fixes for last build

--- EQLogParser/Properties/AssemblyInfo.cs
+++ EQLogParser/Properties/AssemblyInfo.cs
@@ -53,7 +53,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.48")]
-[assembly: AssemblyFileVersion("2.1.48")]
+[assembly: AssemblyVersion("2.1.49")]
+[assembly: AssemblyFileVersion("2.1.49")]
 [assembly: XmlConfigurator(Watch = true)]
 [assembly: SupportedOSPlatform("windows10.0")]
